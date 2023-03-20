a6e71de8f7d68aed91126ec539c1ff2c4536b81a
togby <70891285+togby@users.noreply.github.com>
2023-03-20 16:54:19 +0100

    added slash command and SettingsWindow

--- Core.lua
+++ Core.lua
@@ -3,3 +3,10 @@ local addonName, addon = ...
 HaveGroupWillTravelDB = HaveGroupWillTravelDB or {}
 
 addon:SetupFrames()
+
+
+SLASH_HaveGroupWillTravel1 = "/hgwt"
+SLASH_HaveGroupWillTravel2 = "/HaveGroupWillTravel"
+SlashCmdList["HaveGroupWillTravel"] = function(msg)
+   addon:ShowWindow("SettingsWindow")
+end
