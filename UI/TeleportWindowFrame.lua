17ccecf366c10a5d71a43f434a59f82b10e5bfc3
togby <70891285+togby@users.noreply.github.com>
2023-07-14 22:21:57 +0200

    BackgroundColor.a is now 0.75

--- UI/TeleportWindowFrame.lua
+++ UI/TeleportWindowFrame.lua
@@ -44,7 +44,7 @@ function window:SetupWindow()
 		insets = { left = backdropInsets, right = backdropInsets, top = backdropInsets, bottom = backdropInsets}
 	})
     local BorderColor = {r = 0,g = 0,b = 0, a = 0.85}
-    local BackgroundColor = {r = 0.1,g = 0,b = 0, a = 1}
+    local BackgroundColor = {r = 0.1,g = 0,b = 0, a = 0.75}
 	mainFrame:SetBackdropColor(BackgroundColor.r, BackgroundColor.g, BackgroundColor.b, BackgroundColor.a)
 	mainFrame:SetBackdropBorderColor(BorderColor.r, BorderColor.g, BorderColor.b, BorderColor.a)
 
